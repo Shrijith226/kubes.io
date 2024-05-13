5e471491fad48a389008561dcc43ee9cafca40e3
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2024-05-13 13:51:11 +0530

    commit

--- src/components/AddCustomers/index.tsx
+++ src/components/AddCustomers/index.tsx
@@ -573,11 +573,7 @@ export default function Index({ setToggleAdd }: AddCustomerProps) {
                               heading="Suggestions"
                               className="h-32 overflow-y-scroll"
                             >
-<<<<<<< HEAD
                               {Array.from(Array(26).keys()).map((value) => (
-=======
-                              {[...Array(26).keys()].map((value) => (
->>>>>>> 3fb44b6e2c4056ee6fbd29fb9abea764f41ef89d
                                 <CommandItem
                                   key={value}
                                   value={value.toString()}
@@ -632,11 +628,7 @@ export default function Index({ setToggleAdd }: AddCustomerProps) {
                               heading="Suggestions"
                               className="h-32 overflow-y-scroll"
                             >
-<<<<<<< HEAD
                               {Array.from(Array(26).keys()).map((value) => (
-=======
-                              {[...Array(26).keys()].map((value) => (
->>>>>>> 3fb44b6e2c4056ee6fbd29fb9abea764f41ef89d
                                 <CommandItem
                                   key={value}
                                   value={value.toString()}
